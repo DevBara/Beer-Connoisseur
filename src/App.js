b989c3beab66ef7e7fe63f51d03e0acfd6ef1f0b
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-21 18:15:39 -0500

    imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import BeerAPI from './components/BeerAPI'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <BeerAPI />
         
       </header>
     </div>
