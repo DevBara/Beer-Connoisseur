5d6c41f03b1834e44b662a9faac8430ec8483902
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 09:21:49 -0500

    added how to component

---
+++ src/components/HowTo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class HowTo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>How To Page</h1>
+                
+            </div>
+        )
+    }
+}
