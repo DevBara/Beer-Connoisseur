b4498c4225713e84480237065e83bbed5c1a68a0
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 14:46:27 -0500

    updated Howto page with text

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -4,8 +4,23 @@ export default class HowTo extends Component {
     render() {
         return (
             <div>
-                <h1>How To Page</h1>
-                
+                <h1>How To Use The site</h1>
+                <div>
+                    <ul>
+                        <li>
+                            1. Go to Beer Locator Page
+                        </li>
+                        <li>
+                            2. Click in the search bar
+                        </li>
+                        <li>
+                            3. Type in your favorite beer
+                        </li>
+                        <li>
+                            4. See the results!
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
