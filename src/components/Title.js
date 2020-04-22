7e4d34b473e64455bdbfb55591cf70f50776a607
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 11:13:59 -0500

    added title component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Title extends Component {
+    render() {
+        return (
+            <div className="titleParent">
+                <h1 className="title">Beer Connoisseur</h1>
+                <p className="subtitle">Get info about your favorite beer and even food pairings</p>
+                
+            </div>
+        )
+    }
+}
