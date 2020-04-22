cf1eefcf181f8df3e66b7a3373c16db165e4d251
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 10:35:42 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import BeerLocator from './components/BeerLocator'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
@@ -7,16 +8,30 @@ import HowTo from './components/HowTo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <BeerLocator />
-        <Home />
-        <NavBar />
-        <HowTo />
-        
-      </header>
-    </div>
-  );
+    <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to ="/"> Home</Link>
+              </li>
+              <li>
+                <Link to ="/BeerLocator">Beer Finder</Link>
+              </li>
+              <li>
+                 <Link to ="/HowTo">How To</Link>
+                </li>
+              </ul>
+                </nav>
+                <Switch>
+                    <Route path="/Home" component={Home} />
+                    <Route path="/beerlocator" component={BeerLocator} />
+                    <Route path="/howto" component={HowTo} />
+                </Switch>
+        </div>   
+     </Router>
+   
+  )
 }
 
 export default App;
