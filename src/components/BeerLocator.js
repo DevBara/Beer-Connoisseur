2289c3caf8fd04ac991e5c3f0ce418062466c496
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 11:12:32 -0500

    added text for cleaner look

--- src/components/BeerLocator.js
+++ src/components/BeerLocator.js
@@ -32,11 +32,12 @@ export default class BeerLocator extends Component {
             let beerList = response.data.map(beer =>
                <div key={beer["id"]}>
                     <img src={beer["image_url"]} height="150px" width="100%" />
-                    <h4>{beer["name"]} ({["first_brewed"]}</h4>)
-                    <h5>{beer["tagline"]}</h5>
-                    <h5>{beer["food_pairing"]}</h5>
-                    <h3>{beer["abv"]}</h3>
-                    <p>{beer["description"]}</p>
+                    <h4>Beer Name: {beer["name"]}</h4>
+                    {/* <h5>First Brewed: ({["first_brewed"]}</h5> */}
+                    <h5>Tagline: {beer["tagline"]}</h5>
+                    <h5>What foods go great with this beer: {beer["food_pairing"]}</h5>
+                    <h3>Alcohol content: {beer["abv"]}%</h3>
+                    <p>Description{beer["description"]}</p>
                 </div>);
                 this.setState({
                     beers:response.data,
@@ -55,9 +56,9 @@ export default class BeerLocator extends Component {
         return (
             <div>
                 <div>
-                    {/* {this.state.beersHTML} */}
+                    {this.state.beersHTML}
                 </div>
-                <h1>Beer Finder</h1>
+                
 
             </div>
         )
