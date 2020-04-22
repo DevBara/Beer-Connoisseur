c832d1b5ed693c71d19ba07378d81469e4c8ee92
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 14:35:04 -0500

    used Navlink to redirect on click

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-import Beers from '../images/Beers.jpeg'
 import Sleekbeer from '../images/Sleekbeer.jpg'
+import { NavLink } from 'react-router-dom';
+
+
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="homeParent">
                <div className="beerImage">
                     <img className='beer' src={Sleekbeer} alt='beer cup'></img>
+               </div>
+
+               <div className='mainDescription'>
+                   <h3> Gone are the days of having to guess what is in the beer you are drinking,
+                        not knowing whats in it, and if you are cooking for 2, what beers would go great
+                        with dinner               
+                   </h3>
+                   <h3>
+                       We created this website with that in mind. The objective is to help
+                       beer lovers become even more enthused about BEER! 
+                       Our search too is designed to help you easily find beers so you can either showoff
+                       to your friends or show 
+                   </h3>
+                   <p>Are you ready to locate the perfect beer?</p>
+                   <NavLink to="/beerlocator">Search Beers</NavLink>
                </div>
                 
             </div>
