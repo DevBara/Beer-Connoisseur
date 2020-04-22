21241d4d625a9e45ae1150b945dff0cf71adcff4
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 09:22:11 -0500

    added Nav component

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <NavBar>
+                        <ul>
+                            <li>Home</li>
+                        </ul>
+                        
+                    </NavBar>
+                </div>
+                <h1>Nav Bar</h1>
+                
+            </div>
+        )
+    }
+}
