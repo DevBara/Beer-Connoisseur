802027832b197d0339bc1ea0bdbef1a98278c8e6
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-22 11:14:23 -0500

    corrected error

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import BeerLocator from './components/BeerLocator'
 import Home from './components/Home'
 import HowTo from './components/HowTo'
+import Title from './components/Title'
 
 function App() {
   return (
     <Router>
         <div>
+          <Title />
           <nav>
             <ul>
               <li>
-                <Link to ="/"> Home</Link>
+                <Link className="nav" to ="/home"> Home</Link>
               </li>
               <li>
-                <Link to ="/BeerLocator">Beer Finder</Link>
+                <Link className="nav" to ="/BeerLocator">Beer Finder</Link>
               </li>
               <li>
-                 <Link to ="/HowTo">How To</Link>
+                 <Link className="nav" to ="/HowTo">How To</Link>
               </li>
             </ul>
           </nav>
