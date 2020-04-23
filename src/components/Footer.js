a0eb663b7e798c7d47bdce964d2c48039e57b81b
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-23 11:38:27 -0500

    added text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
+            <div className="footerParent">
+                <div className="footer-child">
+                    <div className='footer-titles-parent'>
+                        <h3 className="footer-titles">Want news of all Things beer?</h3>
+                        <p className="footer-titles">Sign up for our weekly newsletter</p>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="email"></input>
+                        <button type="submit">Submit</button>
+                    </div>
+                   
+                </div>
                 
             </div>
         )
