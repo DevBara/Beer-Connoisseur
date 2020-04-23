289b4839c7734986c98cc175fe3cd7897b32caa8
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-23 16:51:16 -0500

    used table vs div

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,19 @@ import BeerLogo from '../images/BEER_2-512.png'
 export default class Header extends Component {
     render() {
         return (
-            <div className="titleParent">
-               <img src={BeerLogo} alt="beer logo" />
-                <h1 className="title">Beer Connoisseur</h1>
-                <p className="subtitle">Get info about your favorite beer and even food pairings</p>
-                
-            </div>
+            <table className="titleParent">
+                <tbody>
+                    <tr>
+                        <td>
+                            <img className="logo" src={BeerLogo} alt="beer logo" />
+                        </td>
+                        <td>
+                            <h1 className="title">Beer Connoisseur</h1>
+                            <p className="subtitle">Get info about your favorite beer and even food pairings</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         )
     }
 }
