e5ac847a16a2c32ff72c411d89ce311db6222177
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-23 16:47:48 -0500

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import BeerLogo from '../images/BEER_2-512.png'
 
 export default class Header extends Component {
     render() {
         return (
             <div className="titleParent">
+               <img src={BeerLogo} alt="beer logo" />
                 <h1 className="title">Beer Connoisseur</h1>
                 <p className="subtitle">Get info about your favorite beer and even food pairings</p>
                 
