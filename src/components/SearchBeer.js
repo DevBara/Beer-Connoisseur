fe00a78c346f84baf1b96b8e7443619c94dbe4c1
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-24 17:51:41 -0500

    commented out- no search feature

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -1,40 +1,22 @@
-import React, { Component } from 'react'
-import $ from 'jquery'
+// import React, { Component } from 'react'
+// import $ from 'jquery'
 
-export default class SearchBeer extends Component {
-    constructor(props){
-        super(props);
+// function SearchBeer (this.props.) {
 
-        this.state={
-            search:""
-        }
-        
-        this.searchBeer()
-
-    }
-
-    searchBeer(){
-        console.log("Perform search")
-        const url= 'https://api.punkapi.com/v2/beers'
-        $.ajax({
-            url: url,
-            success(searchResults) {
-                console.log("search bar works")
-            },
-            error:(xhr,status,err) => {
-                console.error("search FAILED")
-            }
-        })
-    }
+//     searchBeer(){
+//         console.log("Perform search")
+//         const url= 'https://api.punkapi.com/v2/beers'
+//         $.ajax({
+//             url: url,
+//             success(searchResults) {
+//                 console.log("search bar works")
+//             },
+//             error:(xhr,status,err) => {
+//                 console.error("search FAILED")
+//             }
+//         })
+//     }
+// }
 
 
-    render() {
-      
-        return (
-            <div>
-
-            </div>
-        )
-    }
-  
-}
+ 
