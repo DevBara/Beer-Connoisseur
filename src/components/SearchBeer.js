56119d2e434fcdfa53b426d940670b34f79d7f7f
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-24 11:09:56 -0500

    removed from import may not need

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -10,44 +10,14 @@ export default class SearchBeer extends Component {
         }
 
 
-        $(() => {
-            $('form').on('submit', (event)=>{
-                event.preventDefault();
-        
-                const userInput = $('input[type="text"]').val();
-                let url = "https://api.punkapi.com/v2/beers";
-                
-                let fullUrl = url + userInput;
-        
-                $.ajax({
-                    url: fullUrl
-                }).then( //<<<<<chains the then method
-                    (data)=>{
-                        $('#name').html(data.name);
-                    }, 
-                    ()=>{
-                        console.log("bad request");
-                    }
-                );
-            }); 
-        });
     }
 
 
     render() {
-        let name = document.getElementById('name');
-        let $name = $('#name');
+      
         return (
             <div>
-            <form>
-                <input type="text" placeholder="Beer Name"></input>
-                <input type ="submit" value="Get Beer Info"></input>
-            </form>
-                <dl>
-                    <dt>Beer Name:</dt>
-                    <dd id="name"></dd>
-                    <dt> Description</dt>
-                </dl>
+
             </div>
         )
     }
