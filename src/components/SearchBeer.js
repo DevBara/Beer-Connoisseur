92e34d118543182747e989dbe0a3482b4933f9f4
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-24 10:56:31 -0500

    attempted to use ajax

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
 
-function SearchBeer () {
-    
+export default class SearchBeer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            search:""
+        }
+
+
+        $(() => {
+            $('form').on('submit', (event)=>{
+                event.preventDefault();
+        
+                const userInput = $('input[type="text"]').val();
+                let url = "https://api.punkapi.com/v2/beers";
+                
+                let fullUrl = url + userInput;
+        
+                $.ajax({
+                    url: fullUrl
+                }).then( //<<<<<chains the then method
+                    (data)=>{
+                        $('#name').html(data.name);
+                    }, 
+                    ()=>{
+                        console.log("bad request");
+                    }
+                );
+            }); 
+        });
+    }
+
+
+    render() {
+        let name = document.getElementById('name');
+        let $name = $('#name');
         return (
             <div>
-                <p>dfsfsdf</p>
+            <form>
+                <input type="text" placeholder="Beer Name"></input>
+                <input type ="submit" value="Get Beer Info"></input>
+            </form>
+                <dl>
+                    <dt>Beer Name:</dt>
+                    <dd id="name"></dd>
+                    <dt> Description</dt>
+                </dl>
             </div>
         )
-
+    }
+  
 }
-
-
-
-// function SearchBeer () => { 
-//     ('form').on('submit', (event)=>{
-//         event.preventDefault();
-
-//         const userInput = ('input[type="text"]').val();
-//         let url = "https://api.punkapi.com/v2/beers";
-        
-//         let fullUrl = url + userInput;
-
-//         .ajax({
-//             url: fullUrl
-//         }).then( //<<<<<chains the then method
-//             (data)=>{
-//                 ('#name').html(data.name);
-//                 ('#year').html(data.Year);
-//                 // $('#rating').html(data.Rated);
-//             }, 
-//             ()=>{
-//                 console.log("bad request");
-//             }
-//         );
-//     }); 
-
-export default SearchBeer
